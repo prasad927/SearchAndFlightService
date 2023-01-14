9d0881a69250864cdf0df8f2a54a7aa44eef7903
prasad927 <Prasad.Chaudhari@progress.com>
2023-01-14 13:19:18 +0530

    Added crud classes for basic crud ops

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cityControllers = require("../../controllers/city-controller");
 const FlightController = require("../../controllers/flight-controller");
+const AirportController = require("../../controllers/airport-controller");
 
 const router = express.Router();
 //city api
@@ -16,6 +17,8 @@ router.post(
     '/flights', 
     FlightController.create
 );
-router.get("/flights",FlightController.getAllFlights)
+router.get("/flights",FlightController.getAllFlights);
+
+router.post("/airports",AirportController.create);
 
 module.exports = router;
